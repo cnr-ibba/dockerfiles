db05f917c477f27f8c54429723a6b9ba7807d5fb
Paolo Cozzi <paolo.cozzi@tecnoparco.org>
2015-03-26 14:15:21 +0100

    Working on docker-compose django tutorial
    I'm following the docker compose django documentation, to rise up a
    dajngo website. I've fixed a typo in compose tutorial

--- compose/composetest/app.py
+++ compose/composetest/app.py
@@ -10,6 +10,5 @@ def hello():
 	redis.incr('hits')
 	return 'Hello World! I have been seen %s times.' % redis.get('hits')
 	
-	if __name__ == "__main__":
-		app.run(host="0.0.0.0", debug=True)
-		
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", debug=True)
