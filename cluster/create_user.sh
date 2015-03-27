58bb7ff03fced456b2d2c6d0797dbc8cd663190d
Paolo Cozzi <paolo.cozzi@tecnoparco.org>
2015-03-27 17:32:30 +0100

    Working on NIS client - image generated from centos7-systemd container
    Starting from a centos7-systemd, I've installed all requirements needed
    from a NIS client. The cluster image start from this image and implement
    some specific configuration (like the create_user.sh)

--- cluster/create_user.sh
+++ cluster/create_user.sh
@@ -11,7 +11,8 @@ mkdir -p /home/$USER
 cp /etc/skel/.* /home/$USER/
 echo "alias passwd='yppasswd'" >> /home/$USER/.bashrc
 chown -R $USER:$USER /home/$USER
-chmod -R 700 /home/$USER
+chmod 700 /home/$USER
 
 echo "AllowUsers $USER" >> /etc/ssh/sshd_config
-service sshd restart
+
+systemctl restart sshd.service
