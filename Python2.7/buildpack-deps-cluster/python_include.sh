373a5d8cac94c8e3d6f73e9d8b780a8696bf5905
Paolo Cozzi <paolo.cozzi@tecnoparco.org>
2015-04-14 14:09:41 +0200

    Bug fixed in bunop/cluster-python
    I've installed all prerequesistes in bunop/buildpack-deps-cluster and
    I've fixed instructions to configure python to work with own libraries

---
+++ Python2.7/buildpack-deps-cluster/python_include.sh
@@ -0,0 +1,4 @@
+
+# set C_INCLUDE_PATH and CPP_INCLUDE_PATH for python
+export C_INCLUDE_PATH="/usr/local/include/python2.7:$C_INCLUDE_PATH"
+export CPLUS_INCLUDE_PATH="/usr/local/include/python2.7:$CPLUS_INCLUDE_PATH"
